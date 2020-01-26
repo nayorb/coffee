477cffbdd159078dfad8cf30efc1bf8e6e27f844
nayorb <36243913+nayorb@users.noreply.github.com>
2020-01-26 21:48:36 +0100

    asd

--- sagas/favourites.js
+++ sagas/favourites.js
@@ -1,17 +1,22 @@
 import { call, put, takeEvery, select } from 'redux-saga/effects'
 import { getFavourites, setFavourites } from '../utils/localStorage'
 import { setFavouritesAction, TOGGLE_FAVOURITE } from '../actions/favourites'
+import { selectCoffeeAction } from '../actions/app'
 import { INIT_APP } from '../actions/app'
 
 function* toggleFavouriteSaga({ payload }) {
 	try {
 		const oldFavourites = yield select(state => state.favourites)
+		const selectedCoffee = yield select(state => state.app.selectedCoffee)
 		const fav = oldFavourites.find(f => f === payload.title)
 		let favourites
 		if (fav) {
 			favourites = oldFavourites.filter(f => f !== fav)
 		} else {
 			favourites = [...oldFavourites, payload.title]
+		}
+		if (fav === selectedCoffee) {
+			yield put(selectCoffeeAction(''))
 		}
 		yield setFavourites(favourites)
 		yield put(setFavouritesAction(favourites))
